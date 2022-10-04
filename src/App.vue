7c29eacd152e1db231655ad2c8fd909332e2bf29
Mandrake091 <wasdgamer91@gmail.com>
2022-10-05 01:04:58 +0200

    pokemonAPI-app-vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,35 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div v-for="(artist, el) in artists" :key="el">
+    <p>
+      {{artist.name}}
+    </p>
+  </div>
+  <HelloWorld :artists="artists" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,  
+},
+  data() {
+    return {
+      artists: [
+        { name: "Davido", genre: "afrobeats", country: "Nigeria" },
+        { name: "Burna Boy", genre: "afrobeats", country: "Nigeria" },
+        { name: "AKA", genre: "hiphop", country: "South-Africa" },
+        { name: "Sarkodie", genre: "hiphop", country: "Ghana" },
+      ],
+    };
+  },
+};
 </script>
 
 <style>
